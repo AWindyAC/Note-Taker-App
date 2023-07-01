828936016def22c518c63b2eac9e23df963c0aab
Wendell Fowlkes <fowlkes.wendell@gmail.com>
2023-07-01 14:00:23 -0400

    Deploying to Heroku

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,6 +4,8 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
+const port = process.env.PORT || 3001;
+
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
